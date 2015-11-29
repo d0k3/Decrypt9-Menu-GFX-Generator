8a12ed500c3a8fea1e09e661015f7c3593a7dcdc
d0k3 <tore.anon@gmail.com>
2015-11-29 16:25:44 +0100

    Cleanup & move layering fix to the end

--- main.c
+++ main.c
@@ -2,16 +2,13 @@
 #include "d9menu.h"
 
 #define base_gfx "base_bot.png"
-
-#define entry_active_gfx "entry_active_gfx.png"
-#define entry_gfx_start_x 10
-#define entry_gfx_start_y 38
-#define entry_gfx_step_x 0
-#define entry_gfx_step_y 15
-#define entry_gfx_opacity 80
+#define entry_active_gfx "entry_active.png"
+// #define entry_inactive_gfx "entry_inactive.png"
 
 #define entry_start_x 12
 #define entry_start_y 39
+#define entry_gfx_start_x (entry_start_x - 2)
+#define entry_gfx_start_y (entry_start_y - 1)
 #define entry_step_x 0
 #define entry_step_y 15
 
@@ -27,16 +24,18 @@
 #define menu_font_size 12
 #define menu_active_color "White"
 #define menu_inactive_color "Blue"
+#define menu_active_bg_color "#AA8AC2BF"
+#define menu_inactive_bg_color "transparent"
 
 #define desc_dim_x 130
 #define desc_dim_y 150
-#define desc_gravity "SouthWest"
 #define desc_start_x 180
 #define desc_start_y 50
 
 #define desc_font "Roboto-Bold.ttf"
 #define desc_font_size 11
 #define desc_color "Blue"
+#define desc_gravity "SouthWest"
 
 int main()
 {
@@ -46,15 +45,6 @@ int main()
     printf("set OUTPUT=output\n");
     printf("if not exist %%BUILD%% md %%BUILD%%\n");
     printf("if not exist %%OUTPUT%% md %%OUTPUT%%\n");
-    
-    // create base GFX
-    #ifdef entry_active_gfx
-    printf("\n");
-    for (u32 i = 0; i < MENU_MAX_ENTRIES; i++) {
-        printf("convert -page 320x240+0+0 %s -page +%i+%i %s -flatten %%BUILD%%/base%02i.png\n",
-            base_gfx, entry_gfx_start_x + i * entry_gfx_step_x, entry_gfx_start_y + i * entry_gfx_step_y, entry_active_gfx, i);
-    }
-    #endif
 
     //create label files
     printf("\n");
@@ -87,8 +77,22 @@ int main()
     for (u32 idx_m = 0; menu[idx_m].name != NULL; idx_m++) {
         for (u32 idx_s = 0; idx_s < menu[idx_m].n_entries; idx_s++) {
             // insert entry texts
-            printf( "convert %%BUILD%%/base%02i.png ", idx_s );
+            printf( "convert %s ", base_gfx );
             for (u32 i = 0; i < menu[idx_m].n_entries; i++) {
+                #ifdef entry_active_gfx
+                if (idx_s == i) {
+                    printf( "-draw \"image over %i,%i 0,0 %s\" ",
+                    entry_gfx_start_x + i * entry_step_x, entry_gfx_start_y + i * entry_step_y,
+                    entry_active_gfx);
+                }
+                #endif
+                #ifdef entry_inactive_gfx
+                if (idx_s != i) {
+                    printf( "-draw \"image over %i,%i 0,0 %s\" ",
+                    entry_gfx_start_x + i * entry_step_x, entry_gfx_start_y + i * entry_step_y,
+                    entry_inactive_gfx);
+                }
+                #endif
                 printf( "-draw \"image over %i,%i 0,0 %%BUILD%%/label%04i_%i.png\" ",
                     entry_start_x + i * entry_step_x, entry_start_y + i * entry_step_y,
                     (idx_m * 100) + i, (idx_s == i) ? 1 : 0);
@@ -109,7 +113,7 @@ int main()
             }
             printf( "-gravity North -draw \"image over 0,%i 0,0 %%BUILD%%/mlabel%02i_1.png\" ",
                 menu_start_y, idx_m );
-            printf( "%%OUTPUT%%/menu%04i.png\n", (idx_m * 100) + idx_s );
+            printf( "-flatten %%OUTPUT%%/menu%04i.png\n", (idx_m * 100) + idx_s );
         }
     }
     
